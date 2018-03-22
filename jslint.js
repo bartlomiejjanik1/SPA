d9c8ebfb47918005afa5582ede7ab587a3c919f6
Bartek <bartlomiejjanik1@gmail.com>
2018-03-22 19:18:11 +0100

    first

---
+++ jslint.js
@@ -0,0 +1,11 @@
+// fix the JavaScript code;
+// use JSLint (n++ plug-in or jslint.com)
+var sum = 0;
+var i = 0;
+function calculate() {
+ for (i=1; i < 1001; i++){
+     sum+= i;
+}
+return sum;
+}
+alert("suma to: " + calculate());
